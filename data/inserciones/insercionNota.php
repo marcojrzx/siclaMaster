7b5dd2cfbc0ad584a1728de1b78eb15ab4a9dd96
Daniel Dovali <krewx8@gmail.com>
2014-09-12 18:17:39 -0500

    juntando datos
    Falta la insercion a la base de datos

--- data/inserciones/insercionNota.php
+++ data/inserciones/insercionNota.php
@@ -1,4 +1,21 @@
 <?php
 	require_once("../conexion.php");
-	$datos = json_decode();
+	$datos = json_decode(file_get_contents('php://inputs'), true);
+	$medio = $datos['medio']['idMedio'];
+	$autor = $datos['autor']['idAutor'];
+	$titulo = $datos['titulo'];
+	$sintesis = $datos['sintesis'];
+	$texto = $datos['texto'];
+	$tipo = $datos['tipo'];
+	$pagina = $datos['pagina'];
+	$fecha = $datos['fecha'];
+	$protagonista = $datos['protagonista']['idProtagonista'];
+	$cargo = $datos['cargo']['idCargo'];
+	$pos = $datos['pos-neg'];
+	$tema = $datos['tema']['idTema'];
+	$subtema = $datos['subtema']['idSubtema'];
+	$otro = $datos['otros']['idTema'];
+	$seccion = $datos['seccion'];
+	$municipio = $datos['municipio']['idMunicipio'];
+	$portada = $datos['portada'];
 ?>
