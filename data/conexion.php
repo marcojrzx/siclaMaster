d16961b1021db1f6bed4086ac8757f16109f88ce
krewx <krewx@MacBook-Pro-de-Daniel.local>
2014-11-02 00:28:59 -0600

    Conexión con el servidor

--- data/conexion.php
+++ data/conexion.php
@@ -1,12 +1,16 @@
 <?php  
-	$mysqli = new mysqli("localhost","root","","sicla2");	
+	$mysqli = new mysqli("localhost","root","root","sicla2");
 	if ($mysqli->connect_errno)
 	{
-		$mysqli = new mysqli("localhost","root","root","sicla2");
+		$mysqli = new mysqli("localhost","u270745","201598765M","u270745_sicla2");	
 		if ($mysqli->connect_errno)
 		{
-			echo("Fallo al contenctar a MySQL: (".$mysqli->connect_errno.") ".$mysqli->connect_error);
-			exit();
+			$mysqli = new mysqli("localhost","root","","sicla2");	
+			if ($mysqli->connect_errno)
+			{
+				echo("Fallo al contenctar a MySQL: (".$mysqli->connect_errno.") ".$mysqli->connect_error);
+				exit();
+			}			
 		}
 	}
 ?>
